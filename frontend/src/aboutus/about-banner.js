9b7c16ab028fb3d82be4a9629a05c8a8be489884
mamaleshrh <mamaleshhake@gmail.com>
2023-03-26 13:33:06 +0530

    changes

---
+++ frontend/src/aboutus/about-banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './about-banner.css';
+const Aboutbanner = () => {
+  return (
+    <div className='about-banner'>
+    <h1 style={{fontSize:"5vw",padding:"30px"}}>Global Market<br/> Leader</h1>
+    <p style={{padding:"30px"}}>We’ve found to connect independent traders to the potential of the global stock market since 2021. <br/>Today, we continue to challenge ourselves to provide traders with what they need to succeed. </p>
+    <button className="banner-b123">learn more</button>
+
+    </div>
+  );
+};
+
+export default Aboutbanner;
