e7a2f4c839000b16fb7eac48008f478ac15b1ac5
mamaleshrh <mamaleshhake@gmail.com>
2023-03-28 01:47:09 +0530

    changes

--- frontend/src/SignIn/login.js
+++ frontend/src/SignIn/login.js
@@ -1,111 +1,12 @@
-// import React from "react";
-// import "./signup.css";
-// import logo from "./logo.png";
-// // import { useState, useEffect } from "react";
 
-// const Login = () => {
-//   // const [errorMessages, setErrorMessages] = useState({});
-//   // const [isSubmitted, setIsSubmitted] = useState(false);
-//   // Generate JSX code for error message
+import "./signup.css";
 
-//   // const renderErrorMessage = (name) =>
-//   //   name === errorMessages.name && (
-//   //     <div className="error">{errorMessages.message}</div>
-//   //   );
-//   return (
-//     <div className=" signincont">
-//       <div className="  companyname">
-//         <div className="innerdiv">
-//           <h2>
-//             Need webdesign for your page? designspace will help you... text for
-//             trading app
-//           </h2>
-//           <div style={{ textAlign: "center" }}>
-//             <img src={logo} className="logo" alt="company name"></img>
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="mainsignin">
-//         <div className="innerdiv">
-//           <div style={{ paddingTop: "20vh" }}>
-//             <h2 style={{ textAlign: "center" }}>SIGN IN</h2>
-//             <div className="group">
-//               <div>
-//                 <label htmlFor="email" className="label">
-//                   Email
-//                 </label>
-//               </div>
-//               <div>
-//                 <input
-//                   type="text"
-//                   size="30"
-//                   className="input"
-//                   name="email"
-//                   required
-//                 />
-//               </div>
-//             </div>
-//             <div className="group">
-//               <div>
-//                 <label htmlFor="password" className="label">
-//                   Password
-//                 </label>
-//               </div>
-//               <div>
-//                 <input
-//                   type="password"
-//                   minLength="8"
-//                   className="input"
-//                   name="password"
-//                   required
-//                 />
-//               </div>
-//             </div>
-//             <div className="group">
-//               <button type="button" className="btner">
-//                 LOGIN
-//               </button>
-//             </div>
-//             <div style={{ textAlign: "center" }}>
-//               don't have an account <a href="/signup"><strong>signup here</strong></a>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
 
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-// function Box() {
-//   return (
-//     <mesh>
-//       <boxBufferGeometry attach="geometry" />
-//       <meshLambertMaterial attach="material" color="hotpink" />
-//     </mesh>
-//   );
-// }
-// export default function App() {
-// return (
-// <div className="App">
-// <div className="App-header">
-// <Canvas>
-// <OrbitControls />
-// <ambientLight intensity ={0.5} />
-// <spotLight position={[10, 15,10]} angle={0.3} />
-// <Box />
-// </Canvas>
-// </div>
-// </div>
-// );
-// }
 
 function Model(props) {
   const { scene } = useGLTF("/ethereum/scene.gltf");
@@ -121,7 +22,7 @@ const Login = () => {
   return (
     // <div className="flex flex-row bg-[#2f2f2f] h-[100%]">
     <div className="Login_PAGE flex flex-row bg-[#2f2f2f] h-[100%]">
-      <div className="3d-model w-[50%]">
+      <div className="a3d-model w-[50%]">
         {/* rotate the 3d model */}
         <Canvas
           dpr={[1, 2]}
@@ -205,6 +106,13 @@ const Login = () => {
               Login
             </button>
           </div>
+          <div style={{ textAlign: "center" ,color:"white"}}>
+            dont have an account{" "}
+            <a href="/signup">
+              <strong style={{color:"#0CB1CA"}}>Sign up here</strong>
+            </a>
+          </div>
+
         </div>
       </div>
     </div>
