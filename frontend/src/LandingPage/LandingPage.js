2b77a01c5d7d8d7bacaa52a9d05bef4626ed11a4
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2023-03-28 03:44:03 +0530

    landing

--- frontend/src/LandingPage/LandingPage.js
+++ frontend/src/LandingPage/LandingPage.js
@@ -21,22 +21,41 @@ import React from 'react';
 
 const LandingPage = () => {
   return (
-    <div className=" fixed flex-col justify-center h-[100%] w-[100%] bg-[#091039ff]">
+    <div className="fixed flex-col justify-center h-[100%] w-[100%] bg-[#01084f]">
+      <div className="endsignupButton flex flex-row justify-between mx-[12%] my-8">
+        <div className="heading">
+          <h1 className="text-[#ffffff] text-[4rem] font-bold">
+            Crypto
+            <span className="text-[#ff0000]">Trade</span>
+          </h1>
+        </div>
+        
+        <button className="bg-[#ff0000] text-[#ffffff] font-bold text-[1.5rem] p-5 h-[7%] rounded-full">
+          Sign Up
+        </button>
+      </div>
       <div className="flex flex-row justify-evenly my-[10%]">
         <div className="landing_image h-[50%] w-[50%]">
-          <img className="h-[50%]" src="/animation_500_lfral9jb.gif" alt="" />
+          {/* <iframe src="https://embed.lottiefiles.com/animation/79966"></iframe> */}
+          <img
+            className="h-[50%] m-5 fixed left-[15%] bottom-[25%]"
+            src="/animation_500_lfrdll3f.gif"
+            alt=""
+          />
         </div>
-        <div className="intro">
-          <h1 className="text-5xl font-bold text-[#000000]">
-            Welcome to <br /> <span className="text-[#0CB1CA]">Crypto</span>
-            <span className="text-[#000000]">Market</span>
-          </h1>
-          <p className="text-[#000000]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            condimentum, nisl ut ultricies luctus, nisl nisl aliquet nisl, nec
-            lacinia nunc nisl eget nisl. Sed condimentum, nisl ut ultricies
-            luctus, nisl nisl aliquet nisl, nec lacinia nunc nisl eget nisl.
-          </p>
+        <div className="intro flex flex-col justify-center">
+          <div className="blur_background backdrop-blur-sm bg-[#41404044] m-[10%] rounded-full">
+            <h1 className="text-[#ffffff] text-[3rem] font-bold p-10">
+              Welcome to World's Best
+              <span className="text-[#ff0000]"> Cryptocurrency </span>
+              Trading App
+            </h1>
+          </div>
+          <div className="getStartedButton text-end px-8 relative right-[18%]">
+            <button className="bg-[#ff0000] text-[#ffffff] font-bold text-[1.5rem] p-5 rounded-full">
+              Get Started
+            </button>
+          </div>
         </div>
       </div>
     </div>
