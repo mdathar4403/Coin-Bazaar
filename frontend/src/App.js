d52ecf183b00324d498e6a667d2f629c81806dd5
athar <md.athar.ece21@itbhu.ac.in>
2023-03-27 20:57:11 +0530

    btn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage/LandingPage";
 import Dashboard from "./Dashboard/Dashboard";
-import Login from "./SignIn/login";
+import Login from "./SignIn/Login";
 import Signup from "./SignIn/signup";
 // import Market from "./Market/Market";
 import Coin from "./routes/Coin";
