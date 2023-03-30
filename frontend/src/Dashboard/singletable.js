2c48e29d83dd24c436f60d5e180577f6420f7227
mamaleshrh <mamaleshhake@gmail.com>
2023-03-30 20:23:33 +0530

    dashboard

---
+++ frontend/src/Dashboard/singletable.js
@@ -0,0 +1,22 @@
+import "./Dashboard.css";
+
+import React, { useState, useEffect } from "react";
+
+function Singletable(props) {
+  return (
+
+
+        <tbody>
+              <tr style={{display:'flex',justifyContent:"space-between" }}>
+                <td>{props.stockId}</td>
+                {/* <td>{Math.round(Math.random()*10000+1)}</td> */}
+                <td>{Math.round(props.total_amount)}</td>
+                <td className="war" style={{color:"green"}}>Completed</td>
+                <td className="primary">Details</td>
+              </tr>
+        </tbody>
+
+  );
+}
+
+export default Singletable;
